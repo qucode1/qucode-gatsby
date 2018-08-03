f5d04fad1a6555b15829253378e86756f5ecc806
Yannick Panis <info@qucode.eu>
2018-08-03 23:38:33 +0200

    fix LandingProfileImage.js (image not showing)

--- src/components/LandingCenterSquare.js
+++ src/components/LandingCenterSquare.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled from 'styled-components'
-import Img from 'gatsby-image'
 import { FiGithub, FiTwitter } from 'react-icons/fi'
 
 import LandingProfileImage from './LandingProfileImage'
--- src/components/LandingProfileImage.js
+++ src/components/LandingProfileImage.js
@@ -3,22 +3,28 @@ import styled from 'styled-components'
 import Img from 'gatsby-image'
 
 const LandingProfileImage = ({ className, profileImage }) => (
-  <Img
-    fluid
-    sizes={profileImage.sizes}
-    alt={profileImage.title}
-    className={className}
-    outerWrapperClassName="profileImageOuterWrapper"
-    imgStyle={{
-      left: '-2px',
-      borderRadius: '50%',
-    }}
-  />
+  <div className={className}>
+    <Img
+      fluid
+      sizes={profileImage.sizes}
+      alt={profileImage.title}
+      className="profileImageInnerWrapper"
+      outerWrapperClassName="profileImageOuterWrapper"
+      imgStyle={{
+        left: '-2px',
+        borderRadius: '50%',
+      }}
+    />
+  </div>
 )
 
 export default styled(LandingProfileImage)`
+  width: 100%;
   height: 100%;
-  border-radius: 50%;
+  & .profileImageInnerWrapper {
+    height: 100%;
+    border-radius: 50%;
+  }
   & .profileImageOuterWrapper {
     width: 100%;
     transform: rotate(-45deg);
